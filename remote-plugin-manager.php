bc85d01684cc09b2b9b656d2248b45c2ad6f3ac7
Constantin Kryachko <kostya.kryachko@gmail.com>
2024-10-03 17:03:55 +0300

    Multi lang version Eng and Rus

--- remote-plugin-manager.php
+++ remote-plugin-manager.php
@@ -1,23 +1,32 @@
 <?php
 /*
 Plugin Name: Remote Plugin Manager
-Description: Плагин для загрузки и обновления других плагинов с внешних серверов.
+Description: A plugin for downloading and updating other plugins from external servers.
 Version: 1.0
 Author: Konstantin Kryachko
+Text Domain: remote-plugin-manager
+Domain Path: /languages
 */
+
+// Загрузка текстового домена
+add_action('plugins_loaded', 'rpm_load_textdomain');
+
+function rpm_load_textdomain() {
+    load_plugin_textdomain('remote-plugin-manager', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
 
 // Хук для создания страницы настроек
 add_action('admin_menu', 'rpm_add_admin_menu');
 add_action('admin_init', 'rpm_register_settings');
 
 function rpm_add_admin_menu() {
-    add_options_page('Remote Plugin Manager', 'Remote Plugin Manager', 'manage_options', 'remote-plugin-manager', 'rpm_options_page');
+    add_options_page(__('Remote Plugin Manager', 'remote-plugin-manager'), __('Remote Plugin Manager', 'remote-plugin-manager'), 'manage_options', 'remote-plugin-manager', 'rpm_options_page');
 }
 
 function rpm_register_settings() {
     register_setting('rpm_settings', 'rpm_plugin_urls', array(
         'type' => 'array',
-        'description' => 'URLs for remote plugin download',
+        'description' => __('URLs for remote plugin download', 'remote-plugin-manager'),
         'sanitize_callback' => 'rpm_sanitize_urls',
         'default' => array(),
     ));
@@ -35,7 +44,7 @@ function rpm_sanitize_urls($input) {
 function rpm_options_page() {
 ?>
     <div class="wrap">
-        <h1>Remote Plugin Manager</h1>
+        <h1><?php _e('Remote Plugin Manager', 'remote-plugin-manager'); ?></h1>
         <form method="post" action="options.php">
             <?php
             settings_fields('rpm_settings');
@@ -44,10 +53,10 @@ function rpm_options_page() {
             ?>
             <table class="form-table">
                 <tr valign="top">
-                    <th scope="row">Пути для загрузки плагинов</th>
+                    <th scope="row"><?php _e('Plugin download paths', 'remote-plugin-manager'); ?></th>
                     <td>
                         <textarea name="rpm_plugin_urls" rows="10" cols="50"><?php echo implode("\n", $urls); ?></textarea>
-                        <p class="description">Введите каждый URL на новой строке.</p>
+                        <p class="description"><?php _e('Enter each URL on a new line.', 'remote-plugin-manager'); ?></p>
                     </td>
                 </tr>
             </table>
@@ -82,7 +91,7 @@ function rpm_update_plugins() {
     foreach ($urls as $url) {
         $result = rpm_install_plugin($url);
         if (is_wp_error($result)) {
-            error_log('Ошибка загрузки плагина с ' . $url . ': ' . $result->get_error_message());
+            error_log(__('Error downloading plugin from ', 'remote-plugin-manager') . $url . ': ' . $result->get_error_message());
         }
     }
 }
@@ -92,11 +101,11 @@ add_action('admin_notices', 'rpm_manual_update_button');
 
 function rpm_manual_update_button() {
     if (current_user_can('manage_options')) {
-        echo '<div class="notice notice-success"><p><a href="?rpm_update_plugins=true" class="button">Обновить плагины с удаленных серверов</a></p></div>';
+        echo '<div class="notice notice-success"><p><a href="?rpm_update_plugins=true" class="button">'. __('Update plugins from remote servers', 'remote-plugin-manager') .'</a></p></div>';
     }
 
     if (isset($_GET['rpm_update_plugins'])) {
         rpm_update_plugins();
-        echo '<div class="notice notice-success"><p>Все плагины обновлены.</p></div>';
+        echo '<div class="notice notice-success"><p>'. __('All plugins have been updated.', 'remote-plugin-manager') .'</p></div>';
     }
 }
